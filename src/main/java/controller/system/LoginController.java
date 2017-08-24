9d30863eb3f68a06a2043f619ccd0b0a2f74dd97
lihangeng <1006631522@qq.com>
2017-08-24 15:26:53 +0800

    代码提交

---
+++ src/main/java/controller/system/LoginController.java
@@ -0,0 +1,21 @@
+package controller.system;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public String login(HttpServletRequest req, Model model, HttpSession session) {
+		
+		
+		return null;
+		
+	}
+
+}
---
+++ src/test/java/giit/MainTest.java
@@ -0,0 +1,11 @@
+package giit;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+
+		PrivateTest p = new PrivateTest();
+//		PrivateTest.
+	}
+
+}
